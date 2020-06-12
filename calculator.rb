1a8aeadaed7b8f408ce2a5f50b3ddddc38f27f24
Steven McAllister <steved1mac@gmail.com>
2020-06-12 04:42:07 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,7 @@
-# Add your variables here
+first_number = 3.5 
+first_number.to_f
+second_number = 3
+sum = first_number + second_number 
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number/second_number
