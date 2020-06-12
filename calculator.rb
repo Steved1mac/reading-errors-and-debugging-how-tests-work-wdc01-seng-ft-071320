b84895fe3c44f0f1af5a26eb76725dfba055e5f1
Steven McAllister <steved1mac@gmail.com>
2020-06-12 04:41:34 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,5 +3,5 @@ first_number.to_f
 second_number = 3
 sum = first_number + second_number 
 difference = first_number - second_number
-product = first_number + second_number
+product = first_number * second_number
 quotient = first_number/second_number
